427555004e0a1de09d8eb26014d6c271a8578e73
Mark Bradley <braddle@gmail.com>
2022-10-20 13:14:40 +0100

    Implementing the bowling game kata using Table Driven Tests

--- score.go
+++ score.go
@@ -1,1 +1,26 @@
 package bowling
+
+import (
+	"strconv"
+	"strings"
+)
+
+func Score(game string) int {
+	score := 0
+
+	frames := strings.Split(game, " ")
+
+	for _, frame := range frames {
+		balls := strings.Split(frame, "")
+		if balls[0] != "-" {
+			points, _ := strconv.Atoi(balls[0])
+			score += points
+		}
+
+		if balls[1] != "-" {
+			score++
+		}
+	}
+
+	return score
+}
--- score_test.go
+++ score_test.go
@@ -1,10 +1,50 @@
 package bowling_test
 
 import (
+	"bowling"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestHolding(t *testing.T) {
-	assert.True(t, true)
+func TestGutterBallGame(t *testing.T) {
+	assert.Equal(t, 0, bowling.Score("-- -- -- -- -- -- -- -- -- --"))
+}
+
+func TestFirstPinNumericPoints(t *testing.T) {
+	tests := []struct {
+		name          string
+		game          string
+		expectedScore int
+	}{
+		{"Single pin first frame", "1- -- -- -- -- -- -- -- -- --", 1},
+		{"Single pin first two frames", "1- 1- -- -- -- -- -- -- -- --", 2},
+		{"Single pin first three frames", "1- 1- 1- -- -- -- -- -- -- --", 3},
+		{"First ball simple scores", "-- 1- 2- 3- 4- 5- 6- 7- 8- 9-", 45},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.expectedScore, bowling.Score(test.game))
+		})
+	}
+}
+
+func TestSecondBallNumericPoints(t *testing.T) {
+	tests := []struct {
+		name          string
+		game          string
+		expectedScore int
+	}{
+		{"Single pin first frame", "-1 -- -- -- -- -- -- -- -- --", 1},
+		{"Single pin first two frames", "-1 -1 -- -- -- -- -- -- -- --", 2},
+		{"Single pin first three frames", "-1 -1 -1 -- -- -- -- -- -- --", 3},
+		// TODO add second ball simple score tests
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.expectedScore, bowling.Score(test.game))
+
+		})
+	}
+
+	// TODO start to cover spares and strikes
 }
